3ec90dde627344ec5601350f6c57deb35d842199
OvaLova <candea.ovidiu@yahoo.com>
2025-05-19 16:25:05 +0300

    w3 added

---
+++ w3/model_embeddings.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+CS224N Spring 2024: Assignment 3
+model_embeddings.py: Embeddings for the NMT model
+Pencheng Yin <pcyin@cs.cmu.edu>
+Sahil Chopra <schopra8@stanford.edu>
+Anand Dhoot <anandd@stanford.edu>
+Vera Lin <veralin@stanford.edu>
+Siyan Li <siyanli@stanford.edu>
+"""
+
+import torch.nn as nn
+
+class ModelEmbeddings(nn.Module): 
+    """
+    Class that converts input words to their embeddings.
+    """
+    def __init__(self, embed_size, vocab):
+        """
+        Init the Embedding layers.
+
+        @param embed_size (int): Embedding size (dimensionality)
+        @param vocab (Vocab): Vocabulary object containing src and tgt languages
+                              See vocab.py for documentation.
+        """
+        super(ModelEmbeddings, self).__init__()
+        self.embed_size = embed_size
+
+        # default values
+        self.source = None
+        self.target = None
+
+        src_pad_token_idx = vocab.src['<pad>']
+        tgt_pad_token_idx = vocab.tgt['<pad>']
+
+        ### MY CODE HERE
+        self.source = nn.Embedding(num_embeddings=len(vocab.src), embedding_dim=self.embed_size, padding_idx=src_pad_token_idx)
+        self.target = nn.Embedding(num_embeddings=len(vocab.tgt), embedding_dim=self.embed_size, padding_idx=tgt_pad_token_idx)
+        ### END MY CODE
+
+
